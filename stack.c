6a1d26e69ca1680199dd7b4a1ce52f08ae016834
leolin2810 <91626808+leolin2810@users.noreply.github.com>
2023-05-12 23:01:10 -0400

    Update stack.c
    Framework for all instructions

--- stack.c
+++ stack.c
@@ -1,1 +1,74 @@
+typedef struct
+{
+	int curr_x;
+	int curr_y;
+	int wall_check;
+
+}VA_Action;
+
+typedef struct
+{
+	int x;
+	int y;
+}VA_Memory;
+
+
+int top=-1,VA_memory[Mem_size];
+
+void push
+{
+int x;
+
+    if (top == Mem_size - 1)
+    {
+        printf("\nOverflow!!");
+    }
+    else
+    {
+        printf("\nEnter the element to be added onto the stack: ");
+        scanf("%d", &x);
+        top = top + 1;
+        VA_memory[top] = x;
+    }
+
+};
+
+void pop
+{
+if (top == -1)
+    {
+        printf("\nUnderflow!!");
+    }
+    else
+    {
+        printf("\nPopped element: %d", inp_array[top]);
+        top = top - 1;
+    }
+
+};
+
+Void clear
+{
+
+};
+
+Int CWL
+{
+
+};
+
+Int CWR
+{
+
+};
+
+Int CWF
+{
+
+};
+
+Int CWB
+{
+
+};
 
