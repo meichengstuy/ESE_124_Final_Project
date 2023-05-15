75310631a0f0f59eede9a8f1bcea6b2bf9229b7c
meichengstuy <133471700+meichengstuy@users.noreply.github.com>
2023-05-14 20:26:19 -0400

    Add files via upload
    Added BJPI and CJPI functions.

--- stack.c
+++ stack.c
@@ -1,4 +1,5 @@
-int num_step=0,m;
+int num_step = 0, m;
+int left = right = up = down = 0;
 
 typedef struct
 {
@@ -18,7 +19,7 @@ typedef struct
 }VA_Memory;
 
 
-int top=-1,VA_memory[Mem_size];
+int top =-1 ,VA_memory[Mem_size];
 
 void push()
 {
@@ -54,14 +55,16 @@ if (top == -1)
 
 void clear()
 {
-
+	
 };
 
 int CWL()
 {
+	m=0;
+	left = 1;
 	while (maze[x-1][y] != '*'
 	{
-		m=0;
+		
 		if (maze[x-1][y] == 'x')
 		{
 			VA_Action.check_left[num_step]==m;
@@ -76,6 +79,7 @@ int CWL()
 int CWR()
 {
 	m=0;
+	right = 1;
 	while (maze[x+1][y] != '*'
 	{
 		if (maze[x+1][y] == 'x')
@@ -92,6 +96,7 @@ int CWR()
 int CWD()
 {
 	m=0;
+	down = 1;
 	while (maze[x][y-1] != '*'
 	{
 		if (maze[x][y-1] == 'x')
@@ -108,6 +113,7 @@ int CWD()
 int CWU()
 {	
 	m=0;
+	up = 1;
 	while (maze[x][y+1] != '*'
 	{
 		if (maze[x][y+1] == 'x')
@@ -116,7 +122,7 @@ int CWU()
 		}
 		else
 		{
-			m+=1;
+			m+=1;	
 		}
 	}
 };
@@ -132,27 +138,69 @@ void move_F()
 	VA_Action[num_step].curr_x = VA_Action[num_step+1].curr_x;
 	VA_Action[num_step].curr_y = VA_Action[num_step+1].curr_y + 1;
 };
+
 void move_B()
 {
 	num_step+=1;
 	VA_Action[num_step].curr_x = VA_Action[num_step+1].curr_x;
 	VA_Action[num_step].curr_y = VA_Action[num_step+1].curr_y - 1;
 };
+
 void move_R()
 {
 	num_step+=1;
 	VA_Action[num_step].curr_x = VA_Action[num_step+1].curr_x + 1;
 	VA_Action[num_step].curr_y = VA_Action[num_step+1].curr_y;
 };
+
 void move_L()
 {
 	num_step+=1;
 	VA_Action[num_step].curr_x = VA_Action[num_step+1].curr_x - 1;
 	VA_Action[num_step].curr_y = VA_Action[num_step+1].curr_y;
 };
+
 void BJPI()
 {
+	if (left){
+		for (int i = 0; i < VA_Action.check_left[num_step], i++)
+			move_L();
+		left = 0;
+	}
+	
+	if (right){
+		for (int i = 0; i < VA_Action.check_right[num_step], i++)
+			move_R();
+		right = 0;
+	}
+	if (up){
+		for (int i = 0; i < VA_Action.check_up[num_step], i++)
+			move_F();
+		up = 0;
+	}
+	if (down){
+		for (int i = 0; i < VA_Action.check_down[num_step], i++)
+			move_B();
+		down = 0;
+	}
 };
 void CJPI()
 {
+	if (left){
+		move_L();
+		left = 0;
+	}
+	
+	if (right){
+		move_R();
+		right = 0;
+	}
+	if (up){
+		move_F();
+		up = 0;
+	}
+	if (down){
+		move_B();
+		down = 0;
+	}
 };
